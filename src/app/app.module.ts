6cc31372bc30d9d7de059eeabddf63b13afbdde0
Jim Gallagher <jegallagher@gmail.com>
2017-12-29 12:25:55 +0000

    added showSearch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ShowSearchComponent } from './show-search/show-search.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowSearchComponent
   ],
   imports: [
     BrowserModule,
